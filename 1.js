bb793133dd84aeefb4fdd645d844df6544c7ce6a
YuliaLugovaya <yulialugovayaphoto@gmail.com>
2023-03-29 04:13:12 +0300

    refactor: add solution for task 1

--- 1.js
+++ 1.js
@@ -1,7 +1,7 @@
 // Задача 1
-// Заполните массив следующим образом: в первый элемент запишите '1', 
-// во второй '22', в третий '333' и так далее.
+// Заполните массив следующим образом: в первый элемент запишите '1', во второй '22', в третий '333' и так далее.
 
+// Первоначальное решение
 function newArr(num) {
   let arr = [];
   for (let i = 1; i <= num; i++) {
@@ -11,3 +11,32 @@ function newArr(num) {
   return arr;
 }
 console.log(newArr(5));
+
+// Исправленное решение после созвона по разбору домашки
+const newArrUpdate = (num) => {
+  let arr = [];
+  for (let i = 1; i <= num; i++) {
+    arr.push(String(i).repeat(i));
+  }
+  return arr;
+}
+console.log(newArrUpdate(5));
+
+
+
+// Новые решения после созвона
+
+// Развернуто с помощью new Array
+const newArray = (num) => {
+  let newArr = new Array(num).fill(''); // создаем новый массив длиной, равной num, и применяем метод fill, заполняя массив элементом ""
+  return newArr.map((el, index) => {    // применяем к массиву map с элементом и индексом в параметрах
+    return String(index + 1).repeat(index + 1) // работаем с индексом: берем индекс (на первой итерации ноль), прибавляем 1 и применяем repeat
+  });
+}
+console.log(newArray(5));
+
+// Что-то на сеньорском
+const newArrayHard = (num) => {
+  return new Array(num).fill('').map((el, index) => String(index + 1).repeat(index + 1));
+}
+console.log(newArrayHard(5));
